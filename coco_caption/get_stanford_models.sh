4c0c72a2b5277c125f4d2bb6792dd486c52a9b1f
y00njaekim <rladbswo12@gist.ac.kr>
2024-05-21 08:32:35 +0000

    stanford core nlp 설치 쉘 스크립트 변경

--- coco_caption/get_stanford_models.sh
+++ coco_caption/get_stanford_models.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 # This script downloads the Stanford CoreNLP models.
 
-CORENLP=stanford-corenlp-full-2015-12-09
+CORENLP=stanford-corenlp-latest
 SPICELIB=pycocoevalcap/spice/lib
-JAR=stanford-corenlp-3.6.0
+JAR=stanford-corenlp-4.5.7
 
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $DIR
@@ -12,11 +12,11 @@ if [ -f $SPICELIB/$JAR.jar ]; then
   echo "Found Stanford CoreNLP."
 else
   echo "Downloading..."
-  wget http://nlp.stanford.edu/software/$CORENLP.zip
+  wget http://huggingface.co/stanfordnlp/CoreNLP/resolve/main/$CORENLP.zip
   echo "Unzipping..."
   unzip $CORENLP.zip -d $SPICELIB/
-  mv $SPICELIB/$CORENLP/$JAR.jar $SPICELIB/
-  mv $SPICELIB/$CORENLP/$JAR-models.jar $SPICELIB/
+  mv $SPICELIB/$JAR/$JAR.jar $SPICELIB/
+  mv $SPICELIB/$JAR/$JAR-models.jar $SPICELIB/
   rm -f $CORENLP.zip
   rm -rf $SPICELIB/$CORENLP/
   echo "Done."
